24c00085cb7640f7fa246fd1a53f8653c40fa37d
Mehedi Hasan <designwithmehedi@gmail.com>
2023-11-08 01:57:58 +0600

    Car Show From JSON

--- JS/api-2-data.js
+++ JS/api-2-data.js
@@ -1,34 +1,48 @@
 const data = [
-    {
-      _id: "60795d4e0489a32f948c4167",
-      name: "Honda Sedan",
-      price: "132",
-      description:
-        "Vehicle Type: Sedan, Doors: 2, Seats: 2, Luggage: 2 Suitcases / 2 Bags, Transmission: Automatic",
-      imageURL: "https://i.ibb.co/54WzQjR/Honda.png",
-    },
-    {
-      _id: "60795e440489a32f948c4168",
-      name: "MitoSedan",
-      price: "221",
-      description:
-        "Vehicle Type: Sedan, Doors: 2, Seats: 2, Luggage: 2 Suitcases / 2 Bags, Transmission: Manual",
-      imageURL: "https://i.ibb.co/802Rwsq/Mito.png",
-    },
-    {
-      _id: "60795fc20489a32f948c4169",
-      name: "Isuzu Tacoma",
-      price: "105",
-      description:
-        "Vehicle Type: Pickup Truck, Doors: 5, Seats: 4, Luggage: 6 Suitcases / 8 Bags, Transmission: Manual",
-      imageURL: "https://i.ibb.co/SJK7QYs/isuzu.png",
-    },
-    {
-      _id: "6079615d0489a32f948c416a",
-      name: "Chevrolet Crossover",
-      price: "434",
-      description:
-        "Vehicle Type: Crossover, Doors: 5, Seats: 7, Luggage: 5Suitcases / 5Bags, Transmission: Automatic",
-      
-    },
-  ];
+  {
+    _id: "60795d4e0489a32f948c4167",
+    name: "Honda Sedan",
+    price: "132",
+    description:
+      "Vehicle Type: Sedan, Doors: 2, Seats: 2, Luggage: 2 Suitcases / 2 Bags, Transmission: Automatic",
+    imageURL: "https://i.ibb.co/54WzQjR/Honda.png",
+  },
+  {
+    _id: "60795e440489a32f948c4168",
+    name: "MitoSedan",
+    price: "221",
+    description:
+      "Vehicle Type: Sedan, Doors: 2, Seats: 2, Luggage: 2 Suitcases / 2 Bags, Transmission: Manual",
+    imageURL: "https://i.ibb.co/802Rwsq/Mito.png",
+  },
+  {
+    _id: "60795fc20489a32f948c4169",
+    name: "Isuzu Tacoma",
+    price: "105",
+    description:
+      "Vehicle Type: Pickup Truck, Doors: 5, Seats: 4, Luggage: 6 Suitcases / 8 Bags, Transmission: Manual",
+    imageURL: "https://i.ibb.co/SJK7QYs/isuzu.png",
+  },
+  {
+    _id: "6079615d0489a32f948c416a",
+    name: "Chevrolet Crossover",
+    price: "434",
+    description:
+      "Vehicle Type: Crossover, Doors: 5, Seats: 7, Luggage: 5Suitcases / 5Bags, Transmission: Automatic",
+    imageURL: "https://i.ibb.co/54WzQjR/Honda.png",
+  },
+]; 
+//---------------------------------------------------------------
+const carsContainer = document.getElementById('car-container'); 
+data.map(car => {
+  const carDiv = document.createElement('div');
+  carDiv.classList.add('car')
+  carDiv.innerHTML = `
+    <img src=${car.imageURL}/>
+    <p class="font-semibold text-2xl">Name: ${car.name}</p>
+    <p class="text-xl mt-2 font-semibold text-gray-600">Details: ${car.description}</p>
+    <button class="bg-blue-600 text-white w-1/4 py-2 px-6 rounded mt-5">Price: ${car.price}k</button>
+  
+  `
+  carsContainer.appendChild(carDiv)
+})
