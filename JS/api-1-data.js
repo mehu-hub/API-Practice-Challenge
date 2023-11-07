e3d7def467264d3608bf1ea94f9bc94b6fc6dff5
Mehedi Hasan <designwithmehedi@gmail.com>
2023-11-07 23:09:52 +0600

    JSON Added

--- JS/api-1-data.js
+++ JS/api-1-data.js
@@ -1,30 +1,45 @@
 const person = {
-    found: 2,
-    message: "Found 2 persons",
-    result: [
-      {
-        name: {
-          common: "John",
-          fullName: ["John", "Doe"]
-        },
-        age: 32,
-        isMale: false,
-        address: {
-          street: "13/A St Joseph",
-          house: 10,
-        },
+  found: 2,
+  message: "Found 2 persons",
+  result: [
+    {
+      name: {
+        common: "John",
+        fullName: ["John", "Doe"]
       },
-      {
-        name: {
-          common: "Humayoun",
-          fullName: ["Humayoun", "Kabir"]
-        },
-        age: 33,
-        isMale: false,
-        address: {
-          street: "13/A St Lucia",
-          house: 11,
-        },
+      age: 32,
+      isMale: false,
+      address: {
+        street: "13/A St Joseph",
+        house: 10,
       },
-    ]
-  };
+    },
+    {
+      name: {
+        common: "Humayoun",
+        fullName: ["Humayoun", "Kabir"]
+      },
+      age: 33,
+      isMale: false,
+      address: {
+        street: "13/A St Lucia",
+        house: 11,
+      },
+    },
+  ]
+};
+
+
+const showPerson = () => {
+  const personContainer = document.getElementById('person-container'); 
+     
+      personContainer.innerHTML = `
+      <h2 class="font-semibold text-2xl">Name:${person.result[0].name.fullName}</h2>
+      <p class="font-semibold text-orange-300 text-xl">Age: </p>
+      <p class="font-semibold ">Stret:</p>
+    `
+     
+  
+}
+
+showPerson();
